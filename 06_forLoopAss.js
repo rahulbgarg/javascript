2af02bfcb33829bed582d590264cd454123e3954
rahulbgarg <rbgarg99@gmail.com>
2024-01-05 10:12:20 +0530

    updated with for loop

---
+++ 06_forLoopAss.js
@@ -0,0 +1,29 @@
+console.log(`========= Using For Loop ==============`);
+console.log(`Step 1 : ========= Numbers from 5 to 15 for loop ==============`);
+for (let index = 5; index<=15; index++) {
+    console.log(index);  
+}
+console.log(`Step 2 : ========= Numbers from 50 to 40 for loop ==============`);
+for (let index = 50; index>=40; index--) {
+    console.log(index);  
+}
+console.log(`Step 3 : ========= First 15 Odd Numbers for loop ==============`);
+for (let index = 1; index<=15; index=index+2) {
+    console.log(index);  
+}
+console.log(`Step 4 : ========= First 10 Even Numbers for loop ==============`);
+for (let index = 0; index<=10; index=index+2) {
+    console.log(index);  
+}
+console.log(`Step 5 : ========= Table Of 5 for loop ==============`);
+for (let index = 5; index<=50; index=index+5) {
+    console.log(index);  
+}
+console.log(`Step 6 : ========= Table Of 10 for loop ==============`);
+for (let index = 10; index<=100; index=index+10) {
+    console.log(index);  
+}
+console.log(`Step 7 : ========= Table Of 10 in reverse using for loop ==============`);
+for (let index = 100; index>=10; index=index-10) {
+    console.log(index);  
+}
--- 06_whileAssignment.js
+++ 06_whileAssignment.js
@@ -1,45 +1,47 @@
-console.log(`Step 1 : ========= Numbers from 5 to 15 ==============`);
+console.log(`========= Using While ==============`);
+
+console.log(`Step 1 : ========= Numbers from 5 to 15 Using While ==============`);
 var i=5;
 while (i<=15) {
     console.log(i);
     i++;
 }
 
-console.log(`Step 2 : ========= Numbers from 50 to 40 ==============`);
+console.log(`Step 2 : ========= Numbers from 50 to 40 Using While ==============`);
 var i=50;
 while (i>=40) {
     console.log(i);
     i--;
 }
 
-console.log(`Step 3 : ========= First 15 Odd Numbers ==============`);
+console.log(`Step 3 : ========= First 15 Odd Numbers Using While ==============`);
 var i=1;
 while (i<=15) {
     console.log(i);
     i=i+2;
 }
 
-console.log(`Step 4 : ========= First 11 Even Numbers ==============`);
+console.log(`Step 4 : ========= First 10 Even Numbers Using While ==============`);
 var i=0;
 while (i<=10) {
     console.log(i);
     i=i+2;
 }
 
-console.log(`Step 5 : ========= Table Of 5 ==============`);
+console.log(`Step 5 : ========= Table Of 5 Using While ==============`);
 var i=5;
 while (i<=50) {
     console.log(i);
     i=i+5;
 }
-console.log(`Step 6 : ========= Table Of 10 ==============`);
+console.log(`Step 6 : ========= Table Of 10 Using While ==============`);
 var i=10;
 while (i<=100) {
     console.log(i);
     i=i+10;
 }
 
-console.log(`Step 7 : ========= Table Of 10 in reverse ==============`);
+console.log(`Step 7 : ========= Table Of 10 in reverse Using While ==============`);
 var i=100;
 while (i>=10) {
     console.log(i);
